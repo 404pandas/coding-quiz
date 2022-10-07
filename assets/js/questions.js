d4d8eee4cd5430fbe9e8ca23212822f5311dc56f
Mary Elenius <mary.panda.jackson@gmail.com>
2022-10-07 03:07:36 -0400

    Filled out javascript file for list of questions

---
+++ assets/js/questions.js
@@ -0,0 +1,202 @@
+var questions = [
+    {
+        title: "Sample Question",
+        choices: [
+            "Select this choice",
+            "Choice 2",
+            "Choice 3",
+            "Choice 4",
+        ],
+        answer: "Select this choice"
+    }
+    {
+        title: "Sample Question",
+        choices: [
+            "Choice 1",
+            "Select this choice",
+            "Choice 3",
+            "Choice 4",
+        ],
+        answer: "Select this choice"
+    }
+    {
+    title: "Sample Question",
+    choices: [
+        "Choice 1",
+        "Choice 2",
+        "Select this choice",
+        "Choice 4",
+    ],
+    answer: "Select this choice"
+}
+    {
+    title: "Sample Question",
+    choices: [
+        "Choice 1",
+        "Choice 2",
+        "Choice 3",
+        "Select this choice",
+    ],
+    answer: "Select this choice"
+}
+    {
+    title: "Sample Question",
+    choices: [
+        "Select this choice",
+        "Choice 2",
+        "Choice 3",
+        "Choice 4",
+    ],
+    answer: "Select this choice"
+}
+    {
+    title: "Sample Question",
+    choices: [
+        "Choice 1",
+        "Select this choice",
+        "Choice 3",
+        "Choice 4",
+    ],
+    answer: "Select this choice"
+}
+    {
+    title: "Sample Question",
+    choices: [
+        "Choice 1",
+        "Choice 2",
+        "Select this choice",
+        "Choice 4",
+    ],
+    answer: "Select this choice"
+}
+    {
+    title: "Sample Question",
+    choices: [
+        "Choice 1",
+        "Choice 2",
+        "Choice 3",
+        "Select this choice",
+    ],
+    answer: "Select this choice"
+}
+    {
+    title: "Sample Question",
+    choices: [
+        "Select this choice",
+        "Choice 2",
+        "Choice 3",
+        "Choice 4",
+    ],
+    answer: "Select this choice"
+}
+    {
+    title: "Sample Question",
+    choices: [
+        "Choice 1",
+        "Select this choice",
+        "Choice 3",
+        "Choice 4",
+    ],
+    answer: "Select this choice"
+}
+    {
+    title: "Sample Question",
+    choices: [
+        "Choice 1",
+        "Choice 2",
+        "Select this choice",
+        "Choice 4",
+    ],
+    answer: "Select this choice"
+}
+    {
+    title: "Sample Question",
+    choices: [
+        "Choice 1",
+        "Choice 2",
+        "Choice 3",
+        "Select this choice",
+    ],
+    answer: "Select this choice"
+}
+    {
+    title: "Sample Question",
+    choices: [
+        "Select this choice",
+        "Choice 2",
+        "Choice 3",
+        "Choice 4",
+    ],
+    answer: "Select this choice"
+}
+    {
+    title: "Sample Question",
+    choices: [
+        "Choice 1",
+        "Select this choice",
+        "Choice 3",
+        "Choice 4",
+    ],
+    answer: "Select this choice"
+}
+    {
+    title: "Sample Question",
+    choices: [
+        "Choice 1",
+        "Choice 2",
+        "Select this choice",
+        "Choice 4",
+    ],
+    answer: "Select this choice"
+}
+    {
+    title: "Sample Question",
+    choices: [
+        "Choice 1",
+        "Choice 2",
+        "Choice 3",
+        "Select this choice",
+    ],
+    answer: "Select this choice"
+}
+    {
+    title: "Sample Question",
+    choices: [
+        "Select this choice",
+        "Choice 2",
+        "Choice 3",
+        "Choice 4",
+    ],
+    answer: "Select this choice"
+}
+    {
+    title: "Sample Question",
+    choices: [
+        "Choice 1",
+        "Select this choice",
+        "Choice 3",
+        "Choice 4",
+    ],
+    answer: "Select this choice"
+}
+    {
+    title: "Sample Question",
+    choices: [
+        "Choice 1",
+        "Choice 2",
+        "Select this choice",
+        "Choice 4",
+    ],
+    answer: "Select this choice"
+}
+    {
+    title: "Sample Question",
+    choices: [
+        "Choice 1",
+        "Choice 2",
+        "Choice 3",
+        "Select this choice",
+    ],
+    answer: "Select this choice"
+}
+];
